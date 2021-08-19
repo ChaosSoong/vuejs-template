4b226ef8e715232f6f8ca3fb572f2b98a1ab7917
songweichao <songweichaods@chinamobile.com>
2021-08-19 16:00:04 +0800

    fix: 样式预处理器

--- template/src/App.vue
+++ template/src/App.vue
@@ -11,25 +11,5 @@
   </div>
 </template>
 
-<style {{#if_eq cssPreprocessor "Do not use"}}{{else}} lang="{{cssPreprocessor}}" {{/if_eq}}>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style scoped {{#if_eq cssPreprocessor "Do not use"}}{{else}}lang="{{cssPreprocessor}}"{{/if_eq}}>
 </style>
