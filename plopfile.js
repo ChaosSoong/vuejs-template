063034dfacee47eeeb0faaab14678cd2e2c4c244
songweichao <songweichaods@chinamobile.com>
2021-09-07 11:20:43 +0800

    feat: plop生成模板

---
+++ plopfile.js
@@ -0,0 +1,21 @@
+module.exports = plop => {
+  plop.setGenerator("pages", {
+    // 这里设定的名字，在执行命令行的时候会用到
+    description: "create the repeat pages", // 这里是对这个plop的功能描述
+    prompts: [
+      {
+        type: "input", // 问题的类型
+        name: "fileName", // 问题对应得到答案的变量名，可以在actions中使用该变量
+        message: "your fileName is", // 在命令行中的问题
+        default: "page" // 默认值
+      }
+    ],
+    actions: [
+      {
+        type: "add", // 操作类型，这里是添加文件
+        path: "./src/views/{{fileName}}.vue", // 添加的文件的路径
+        templateFile: "plop-temp/temp.vue" // 模板文件的路径
+      }
+    ]
+  });
+};
---
+++ template/plop-temp/temp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "{{fileName}}",
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  filters: {},
+  created() {},
+  mounted() {},
+  destroyed() {},
+  methods: {}
+};
+</script>
+<style></style>
